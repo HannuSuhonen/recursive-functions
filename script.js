20133ab5f5cb427a1c165caa7ae04734fb592f1f
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-10-28 22:58:22 +0200

    add factorial recursive

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("hello world!");
-
 function sumRangeRecursive(n){
     if(n === 0){
         return 0;
@@ -8,19 +6,19 @@ function sumRangeRecursive(n){
     }
 }
 
-function sumRange(n = 3){
-    let total = 0;
-    for(let i = 0; i <= n; i++){
-        total += i;
-    }
-    return total;
-}
-
 function powRecursive(base, exponent){
     if(exponent === 1){
         return base;
     }
     else{
         return base * powRecursive(base,exponent - 1);
+    }
+}
+
+function factorialRecursive(n){
+    if(n === 1){
+        return 1;
+    }else{
+        return n * factorialRecursive(n - 1);
     }
 }
